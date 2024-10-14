78efa8f2d9fb50fbc6067af4f2f7913db93bbb05
Yogi Arismet <arismetyogi@gmail.com>
2024-10-14 08:34:31 +0700

    first commit

---
+++ app/Filament/Resources/EmployeeResource.php
@@ -0,0 +1,323 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EmployeeResource\Pages;
+use App\Filament\Resources\EmployeeResource\RelationManagers;
+use App\Models\Employee;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EmployeeResource extends Resource
+{
+  protected static ?string $model = Employee::class;
+
+  protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+  public static function form(Form $form): Form
+  {
+    return $form
+      ->schema([
+        Forms\Components\Section::make('Location')
+          ->description('Select Corresponding User Location')
+          ->schema([
+            Forms\Components\TextInput::make('country_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('state_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('city_id')
+              ->required()
+              ->numeric(),
+          ]),
+        Forms\Components\Section::make('Employees\' Personal Information')
+          ->schema([
+            Forms\Components\TextInput::make('NIK')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('first_name')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('middle_name')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('last_name')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('city_of_birth')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\DatePicker::make('date_of_birth')
+              ->required(),
+            Forms\Components\TextInput::make('phone_no')
+              ->tel()
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('sex')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('address')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('zip_code')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('npwp')
+              ->required()
+              ->maxLength(255),
+          ])->columns(3),
+        Forms\Components\Section::make('Employees\' Job Information')
+          ->schema([
+            Forms\Components\TextInput::make('department_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('payroll_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('employee_status')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('title_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('subtitle_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('band')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('outlet_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('npp')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('gradeeselon_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('area_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('emplevel_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('saptitle_id')
+              ->required()
+              ->numeric(),
+            Forms\Components\DatePicker::make('date_hired')
+              ->required(),
+            Forms\Components\DatePicker::make('date_promoted')
+              ->required(),
+            Forms\Components\DatePicker::make('date_last_mutated')
+              ->required(),
+            Forms\Components\TextInput::make('descstatus_id')
+              ->required()
+              ->numeric(),
+          ])->columns(3),
+        Forms\Components\Section::make('Section Name')
+          ->description('Section descriptions')
+          ->schema([
+            Forms\Components\TextInput::make('bpjs_id')
+              ->required()
+              ->maxLength(255),
+            Forms\Components\TextInput::make('insured_member_count')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('bpjs_class')
+              ->required()
+              ->numeric(),
+            Forms\Components\TextInput::make('bpjstk_id')
+              ->required()
+              ->numeric(),
+          ])->columns(3),
+        Forms\Components\TextInput::make('contract_id')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('tax_id')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('honorarium')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('rekening_no')
+          ->required()
+          ->maxLength(255),
+        Forms\Components\TextInput::make('rekening_name')
+          ->required()
+          ->maxLength(255),
+        Forms\Components\TextInput::make('bank_id')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('recruitment_id')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('pants_size')
+          ->required()
+          ->maxLength(255),
+        Forms\Components\TextInput::make('shirt_size')
+          ->required()
+          ->maxLength(255),
+      ])->columns(3);
+  }
+
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->columns([
+        Tables\Columns\TextColumn::make('country_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('state_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('city_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('department_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('payroll_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('NIK')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('first_name')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('middle_name')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('last_name')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('city_of_birth')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('date_of_birth')
+          ->date()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('phone_no')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('sex')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('address')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('zip_code')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('npwp')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('employee_status')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('title_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('subtitle_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('band')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('outlet_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('npp')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('gradeeselon_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('area_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('emplevel_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('saptitle_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('date_hired')
+          ->date()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('date_promoted')
+          ->date()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('date_last_mutated')
+          ->date()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('descstatus_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('bpjs_id')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('insured_member_count')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('bpjs_class')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('bpjstk_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('contract_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('tax_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('honorarium')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('rekening_no')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('rekening_name')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('bank_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('recruitment_id')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('pants_size')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('shirt_size')
+          ->searchable(),
+        Tables\Columns\TextColumn::make('created_at')
+          ->dateTime()
+          ->sortable()
+          ->toggleable(isToggledHiddenByDefault: true),
+        Tables\Columns\TextColumn::make('updated_at')
+          ->dateTime()
+          ->sortable()
+          ->toggleable(isToggledHiddenByDefault: true),
+      ])
+      ->filters([
+        //
+      ])
+      ->actions([
+        Tables\Actions\ViewAction::make(),
+        Tables\Actions\EditAction::make(),
+      ])
+      ->bulkActions([
+        Tables\Actions\BulkActionGroup::make([
+          Tables\Actions\DeleteBulkAction::make(),
+        ]),
+      ]);
+  }
+
+  public static function getRelations(): array
+  {
+    return [
+      //
+    ];
+  }
+
+  public static function getPages(): array
+  {
+    return [
+      'index' => Pages\ListEmployees::route('/'),
+      'create' => Pages\CreateEmployee::route('/create'),
+      'view' => Pages\ViewEmployee::route('/{record}'),
+      'edit' => Pages\EditEmployee::route('/{record}/edit'),
+    ];
+  }
+}
---
+++ database/migrations/2024_10_12_144851_create_employees_table.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('employees', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('country_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('state_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('department_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('payroll_id')->constrained()->cascadeOnDelete();
+      $table->char('NIK');
+      $table->string('first_name');
+      $table->string('middle_name');
+      $table->string('last_name');
+      $table->string('city_of_birth');
+      $table->date('date_of_birth');
+      $table->char('phone_no');
+      $table->string('sex');
+      $table->string('address');
+      $table->char('zip_code');
+      $table->char('npwp');
+      $table->string('employee_status');
+      $table->foreignId('title_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('subtitle_id')->constrained()->cascadeOnDelete();
+      $table->string('band');
+
+      $table->foreignId('outlet_id')->constrained()->cascadeOnDelete();
+      $table->string('npp');
+      $table->foreignId('gradeeselon_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('area_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('emplevel_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('saptitle_id')->constrained()->cascadeOnDelete();
+      $table->date('date_hired');
+      $table->date('date_promoted');
+      $table->date('date_last_mutated');
+      $table->foreignId('descstatus_id')->constrained()->cascadeOnDelete();
+
+      $table->char('bpjs_id');
+      $table->integer('insured_member_count');
+      $table->integer('bpjs_class');
+      $table->integer('bpjstk_id');
+
+      $table->string('contract_document_id');
+      $table->integer('contract_sequence_no');
+      $table->integer('contract_term');
+      $table->date('contract_start');
+      $table->date('contract_end');
+
+      $table->string('tax_id');
+      $table->string('status_pasangan'); // TK, K-0, K-1, K-2, K-3
+      $table->integer('jumlah_tanggungan'); // (0-3)
+      $table->string('pasangan_ditanggung_pajak'); // (ya/tidak)
+
+      $table->integer('honorarium');
+      $table->char('rekening_no');
+      $table->string('rekening_name');
+      $table->foreignId('bank_id')->constrained()->cascadeOnDelete();
+
+      $table->foreignId('recruitment_id')->constrained()->cascadeOnDelete();
+
+      $table->string('pants_size');
+      $table->string('shirt_size');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('employees');
+  }
+};
