1ae5c448981529fdca647dbfbd6bb538b8a73c0e
Yogi Arismet <arismetyogi@gmail.com>
2024-10-15 05:02:45 +0700

    update model relationships

--- app/Models/State.php
+++ app/Models/State.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class State extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  public function cities(): HasMany
+  {
+    return $this->hasMany(City::class);
+  }
+
+  public function employees(): HasMany
+  {
+    return $this->hasMany(Employee::class);
+  }
 }
