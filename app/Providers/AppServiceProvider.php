c66e5c39e361ce2cfd17d80d73cfb636b1c04252
arismetyogi <arismetyogi@gmail.com>
2024-11-22 14:31:54 +0700

    update rate limiter to 20/minute

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,33 +9,33 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-  /**
-   * Register any application services.
-   */
-  public function register(): void
-  {
-    //
-  }
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-  /**
-   * Bootstrap any application services.
-   */
-  public function boot(): void
-  {
-    RateLimiter::for('api', function (Request $request) {
-      return Limit::perMinute(10)->by($request->user()?->id ?: $request->ip());
-    });
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(maxAttempts: 10)->by($request->user()?->id ?: $request->ip());
+        });
 
-    RateLimiter::for('global', function (Request $request) {
-      return Limit::perMinute(100)->response(function (Request $request, array $headers) {
-        return response('Request exceeded...', 429, $headers);
-      });
-    });
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(20)->response(function (Request $request, array $headers) {
+                return response('Request exceeded...', 429, $headers);
+            });
+        });
 
-    RateLimiter::for('uploads', function (Request $request) {
-      return $request->user()
-        ? Limit::perMinute(100)->by($request->user()->id)
-        : Limit::perMinute(10)->by($request->ip());
-    });
-  }
+        RateLimiter::for('uploads', function (Request $request) {
+            return $request->user()
+                ? Limit::perMinute(20)->by($request->user()->id)
+                : Limit::perMinute(10)->by($request->ip());
+        });
+    }
 }
